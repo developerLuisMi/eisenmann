529b39ab15f61fd6651d75a1d145be93226ebda6
developerLuisMi <desing.web.lm@gmail.com>
2024-02-09 18:01:25 -0500

    texto eliminado de git

--- front/src/componentes/Title.jsx
+++ front/src/componentes/Title.jsx
@@ -2,7 +2,7 @@ const Title = () => {
   return (
     <div className="title1">
       <div className="estructuras" id="services">
-        <h1>Estructuras Metalicas en la empresa Eisenmann</h1>
+        <h1>Estructuras Metalicas</h1>
         <div className="fabricacion">
           <h3>Fabricacion y reparacion de carrocerias</h3>
         </div>
